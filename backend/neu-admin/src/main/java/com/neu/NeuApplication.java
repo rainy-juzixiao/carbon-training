c781546fdae940af4246ddfd1461e4da091ca79d
rainy_juzixiao <aTzXQR8w96I@outlook.com>
2026-09-24 14:44:37 +0800

    Added Start hint.

--- backend/neu-admin/src/main/java/com/neu/NeuApplication.java
+++ backend/neu-admin/src/main/java/com/neu/NeuApplication.java
@@ -6,18 +6,14 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
- * 
+ *
  * @author neuedu
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class NeuApplication
-{
-    private static String s;
-
-    public static void main(String[] args)
-    {
-         System.setProperty("spring.devtools.restart.enabled", "false");
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class NeuApplication {
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(NeuApplication.class, args);
-        System.out.println(s);
+        System.out.println("Let's Getting Fucking Around");
     }
 }
